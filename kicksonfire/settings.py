53bcb08a1a8dde2eddf67db73e5809e6cc2b2437
Ciaran Griffin <griffinciaran3@gmail.com>
2024-09-24 14:30:02 +0000

    Create secert_key settings

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t588qhc_5_b$fyw!y6*4ohe!rfwiav#@veq*fpk@*dy$mw0euv'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
