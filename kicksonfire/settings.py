0947eb8d50162cdec5be33854d4df94ce19d5f67
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-03 19:53:34 +0000

    Added sizes to products model

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-svmxzvu18se.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-w5ftimoeat5.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-kicksonfire-svmxzvu18se.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-kicksonfire-w5ftimoeat5.ws.codeinstitute-ide.net'
 ]
 
 
