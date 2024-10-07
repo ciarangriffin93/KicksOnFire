3425f24018d1ed388b65d48f466672888a815b25
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-07 15:10:16 +0000

    Added checkout views and templates

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -53,6 +53,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +71,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'kicksonfire.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -82,8 +87,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
