8f09d1015ce68129b419e289842d5575535f4a7d
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-07 09:36:58 +0000

    Added toast message

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-3rqzqdovnwx.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-7qgws2pcc9f.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-kicksonfire-3rqzqdovnwx.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-kicksonfire-7qgws2pcc9f.ws.codeinstitute-ide.net'
 ]
 
 
