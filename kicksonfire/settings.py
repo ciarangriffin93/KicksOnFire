b8570d6c79b24bad84af4a688951ca6c83c9d32c
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-07 17:05:22 +0000

    Created button on card in home page

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-7tgb6e7gcse.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-i47crg8xs40.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-ciarangriff-kicksonfire-7tgb6e7gcse.ws.codeinstitute-ide.net'
