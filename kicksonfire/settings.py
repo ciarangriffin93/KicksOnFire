b1b9c90b52f3916d3e20bfeef7db1eee25d4eb7c
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-09 11:40:03 +0000

    Created vaild errors on the card element

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-eh6m98y2upn.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-1a2dka11xnh.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-kicksonfire-eh6m98y2upn.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-kicksonfire-1a2dka11xnh.ws.codeinstitute-ide.net'
 ]
 
 
