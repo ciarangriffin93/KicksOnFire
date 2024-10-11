23e6afdebcfaba24c2c66e8c97bf120c231bc418
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-11 11:07:04 +0000

    Added profiles on file

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-bygzy3mmwbz.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-phrheh6kobu.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-kicksonfire-bygzy3mmwbz.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-kicksonfire-phrheh6kobu.ws.codeinstitute-ide.net'
 ]
 
 
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'profiles',
 
     # Other
     'crispy_forms',
