ee6b6c0a78542333a18dabd836baf30ea83045f2
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-14 14:58:53 +0000

    Finished product functionality

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-r5kg8v2egey.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-31qqifnyv5t.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-kicksonfire-1b1003ofj1v.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-kicksonfire-31qqifnyv5t.ws.codeinstitute-ide.net'
 ]
 
 
