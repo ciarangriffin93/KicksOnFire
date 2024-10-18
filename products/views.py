75a6a599ecd7ed0e180438c183f19c5066074999
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-18 14:59:01 +0000

    Added deleting product

--- products/views.py
+++ products/views.py
@@ -72,14 +72,14 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()  # Save the form and assign the product instance
             messages.success(request, 'Product added successfully!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))  # Use the saved product's id
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
@@ -109,3 +109,10 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
