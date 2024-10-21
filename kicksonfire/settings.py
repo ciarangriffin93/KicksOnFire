2db162e205bbff3c6e0c80324f6ee933004f3f77
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-21 09:40:20 +0000

    Added widgets and fix the images field

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-46fxbawtrtw.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-t7k3yxadlk7.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-kicksonfire-46fxbawtrtw.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-kicksonfire-t7k3yxadlk7.ws.codeinstitute-ide.net'
 ]
 
 
