0c5e5e0f14788c16295296deea42495115057163
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-23 11:34:12 +0000

    Created review models

--- review/models.py
+++ review/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    # Other product fields...
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(default=1)  # Rating out of 5
+    review_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review by {self.user.username} on {self.product.name}'
