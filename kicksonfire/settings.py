e0cff6065bd89cb001615f8015cbf2bc5ebc368f
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-24 09:25:26 +0000

    Created review on css in product detail

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-le201ybnqzm.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-fxs0rx10eje.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-kicksonfire-le201ybnqzm.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-kicksonfire-fxs0rx10eje.ws.codeinstitute-ide.net'
 ]
 
 
