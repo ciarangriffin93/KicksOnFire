39b64a24353b0405338af6d317a16393c65a267d
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-24 14:03:35 +0000

    Remove @login_required in product view

--- products/views.py
+++ products/views.py
@@ -78,7 +78,7 @@ def product_detail(request, product_id):
     }
     return render(request, 'products/product_detail.html', context)
 
-@login_required
+
 def add_product(request):
     """ Add a product to the store """
     if not request.user.is_superuser:
@@ -103,7 +103,6 @@ def add_product(request):
 
     return render(request, template, context)
 
-@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
 
@@ -132,7 +131,6 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 
-@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
