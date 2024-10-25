733b6b971b8f717288a60a0f17cd6cb346e33086
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-25 13:40:20 +0000

    Fixed classes for responsivenes in home page

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-9r60qadk54b.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-egunhnqccd3.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-ciarangriff-kicksonfire-9r60qadk54b.ws.codeinstitute-ide.net'
