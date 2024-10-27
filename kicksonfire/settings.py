dc373d37cc92faa025fa7d032d6befde460d9a4c
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-27 13:46:08 +0000

    Remove fixed-top in base.html

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-4kwpsjv2dd6.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-4exo4vve1w2.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-ciarangriff-kicksonfire-4kwpsjv2dd6.ws.codeinstitute-ide.net'
