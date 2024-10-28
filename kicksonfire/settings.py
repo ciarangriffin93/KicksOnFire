8b3e192ba509c87135fca68c56ce71884372275d
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-28 17:49:25 +0000

    fix navbar in main_nav.html

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-p8oj9c6uv3l.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-ymb4usobx18.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-ciarangriff-kicksonfire-p8oj9c6uv3l.ws.codeinstitute-ide.net'
