b803b33cede3e4c0f82a1b2219a9bfc6fd57bcc6
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-28 11:51:08 +0000

    Add blog post model and admin

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-4exo4vve1w2.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-p8oj9c6uv3l.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-kicksonfire-4exo4vve1w2.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-kicksonfire-p8oj9c6uv3l.ws.codeinstitute-ide.net'
 ]
 
 
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'profiles',
     'wishlist',
     'review',
+    'blog',
 
     # Other
     'crispy_forms',
