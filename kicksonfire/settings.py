66709a00c786b4d8a2e74c0e8677b23cf62ca5c9
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-30 18:21:41 +0000

    Remove review on setting

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-vy8tfa2c1h2.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-bejrfeq9qnj.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-ciarangriff-kicksonfire-vy8tfa2c1h2.ws.codeinstitute-ide.net'
@@ -55,7 +55,6 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'wishlist',
-    'review',
     'blog',
 
     # Other
