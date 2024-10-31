c07d5efb4613afb425eca8672c8357d2bb9e13aa
Ciaran Griffin <griffinciaran3@gmail.com>
2024-10-31 20:54:34 +0000

    Fix quantity input +/- buttons

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-0x9d2g6kupc.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-wcjnbbze8fk.ws.codeinstitute-ide.net','.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-kicksonfire-0x9d2g6kupc.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-kicksonfire-wcjnbbze8fk.ws.codeinstitute-ide.net'
 ]
 
 
