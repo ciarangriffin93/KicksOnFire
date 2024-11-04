74872eae7622f28448881a74f6dc748ead8d9d8d
Ciaran Griffin <griffinciaran3@gmail.com>
2024-11-04 14:28:02 +0000

    Fix space between navbar header for mobile and ipad

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-cujnujy6x2n.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-jd90ziswo0x.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-ciarangriff-kicksonfire-cujnujy6x2n.ws.codeinstitute-ide.net'
