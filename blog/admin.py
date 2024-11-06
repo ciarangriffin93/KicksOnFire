9b7f377a1334cd03374264eb6ebed1b85344cf22
Ciaran Griffin <griffinciaran3@gmail.com>
2024-11-06 18:05:00 +0000

    Add blog comment, models and admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
@@ -10,3 +10,5 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('content',)
+
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="blog_comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_commenter_authors")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
