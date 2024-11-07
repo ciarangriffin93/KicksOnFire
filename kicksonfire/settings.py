bad68aa19f130d01c2620645ed2e402e366fe8d4
Ciaran Griffin <griffinciaran3@gmail.com>
2024-11-07 14:16:12 +0000

    Create sending real email with django

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-7ooroddeqb6.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-c96ogdodfj5.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-kicksonfire-7ooroddeqb6.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-kicksonfire-c96ogdodfj5.ws.codeinstitute-ide.net'
 ]
 
 
@@ -216,4 +216,15 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'KicksOnFire@example.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'KicksOnFire@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER''')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS''')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER''')
