e84eaed65214993e96712e9ae451b20a42f953a1
Ciaran Griffin <griffinciaran3@gmail.com>
2024-11-08 14:36:16 +0000

    Fix checkout_success page without error reguire login in views

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpR
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
+from django.core.mail import send_mail
+from .models import Order
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
@@ -134,7 +138,6 @@ def checkout(request):
 
     return render(request, template, context)
 
-
 def checkout_success(request, order_number):
     """
     Handle successful checkouts
@@ -142,25 +145,32 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    profile = UserProfile.objects.get(user=request.user)
-    # Attach the user's profile to the order
-    order.user_profile = profile
-    order.save()
+    # Only attach user profile if the user is authenticated
+    if request.user.is_authenticated:
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            # Attach the user's profile to the order
+            order.user_profile = profile
+            order.save()
 
-    # Save the user's info
-    if save_info:
-        profile_data = {
-            'default_phone_number': order.phone_number,
-            'default_country': order.country,
-            'default_postcode': order.postcode,
-            'default_town_or_city': order.town_or_city,
-            'default_street_address1': order.street_address1,
-            'default_street_address2': order.street_address2,
-            'default_county': order.county,
-        }
-        user_profile_form = UserProfileForm(profile_data, instance=profile)
-        if user_profile_form.is_valid():
-            user_profile_form.save()
+            if save_info:
+                profile_data = {
+                    'default_phone_number': order.phone_number,
+                    'default_country': order.country,
+                    'default_postcode': order.postcode,
+                    'default_town_or_city': order.town_or_city,
+                    'default_street_address1': order.street_address1,
+                    'default_street_address2': order.street_address2,
+                    'default_county': order.county,
+                }
+                user_profile_form = UserProfileForm(profile_data, instance=profile)
+                if user_profile_form.is_valid():
+                    user_profile_form.save()
+        except UserProfile.DoesNotExist:
+            messages.error(request, "User profile does not exist.")
+            return redirect('home')
+    else:
+        messages.info(request, "You are not logged in. Profile information was not saved.")
 
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
