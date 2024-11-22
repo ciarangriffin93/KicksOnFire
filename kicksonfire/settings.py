3f52b523da692dccc1eadf0669b8031d365cbf0f
Ciaran Griffin <griffinciaran3@gmail.com>
2024-11-22 11:53:59 +0000

    remove the highlighted part in the href link

--- kicksonfire/settings.py
+++ kicksonfire/settings.py
@@ -30,13 +30,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-z8001gfymk8.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-ciarangriff-kicksonfire-qc73ljvwk17.ws.codeinstitute-ide.net']
 host = os.environ.get("HOST")
 if host:
     ALLOWED_HOSTS.append(host)
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-kicksonfire-z8001gfymk8.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-kicksonfire-qc73ljvwk17.ws.codeinstitute-ide.net'
 ]
 
 # Application definition
